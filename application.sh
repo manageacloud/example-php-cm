5bd798cc2f337dcac2d1d713489143033bbdc29b
Ruben Rubio Rey <ruben@manageacloud.com>
2015-06-02 18:17:56 +1000

    Added some comments and a parameter to switch branch

--- application.sh
+++ application.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+# install apache and php
 apt-get update -qy
 apt-get install apache2 libapache2-mod-php5 -y
 
+# Read the system environment variables with the parameters
 DB_IP=${DB_IP:-127.0.0.1}
+APP_BRANCH=${APP_BRANCH:-master}
 
+# clone the git repository that contains the application
 cd /var/www/
-
 git clone -q https://github.com/manageacloud/example-php.git
 
+# checkout the required branch
+cd /var/www/example-php/
+git checkout origin/$APP_BRANCH
+
+# configure the virtual host
 cat >/etc/apache2/sites-enabled/example.conf <<EOL
 <VirtualHost _default_:80>
 DocumentRoot /var/www/example-php
